5fbccb8e853883d227ebed80eb2fafa932881540
foleyb25 <foleyb25@gmail.com>
2023-08-15 16:25:30 -0600

    Revert "Fixing button clicks on mobile - layering issue"
    This reverts commit db8e24833cc425ffa4dac832ccb92e8a713119ee.

--- src/layouts/MobileNavBar.jsx
+++ src/layouts/MobileNavBar.jsx
@@ -23,9 +23,9 @@ const MobileNavBar = () => {
   ];
 
   return (
-    <div className="relative">
+    <div className="relative z-20">
       <button
-        className="fixed z-30 px-4 py-2 top-5 right-5 border border-white rounded-lg"
+        className="fixed px-4 py-2 top-5 right-5 z-30 border border-white rounded-lg"
         onClick={() => setIsDrawerOpen(!isDrawerOpen)}
       >
         <FontAwesomeIcon
@@ -33,69 +33,75 @@ const MobileNavBar = () => {
           icon={isDrawerOpen ? faTimes : faBars}
         />
       </button>
-      {/* Backdrop */}
-      {isDrawerOpen && (
+
+      <div className="fixed inset-0 z-10">
+        {/* Backdrop */}
         <div
-          className={`z-10 fixed inset-0 bg-black opacity-50 transition-opacity duration-300`}
+          className={`absolute inset-0 bg-black ${
+            isDrawerOpen ? 'opacity-50' : 'opacity-0 pointer-events-none'
+          } transition-opacity duration-300 bg-blur-lg`}
           onClick={() => setIsDrawerOpen(false)}
         ></div>
-      )}
 
-      {/* Drawer */}
+        {/* Drawer */}
 
-      <div
-        className={`fixed z-30 top-0 left-0 h-screen w-64 py-4 bg-primary text-primary flex flex-col transition-transform transform duration-300 ${
-          isDrawerOpen ? 'translate-x-0' : '-translate-x-full'
-        } overflow-y-auto`}
-      >
-        <Link
-          to="/"
-          className="text-2xl font-bold mb-4 flex justify-center items-center"
+        <div
+          className={`fixed top-0 left-0 h-screen w-64 py-4 bg-primary text-primary flex flex-col transition-transform transform duration-300 ${
+            isDrawerOpen ? 'translate-x-0' : '-translate-x-full'
+          } overflow-y-auto`}
         >
-          <img
-            src={isDarkMode ? darkModeLogo : lightModeLogo}
-            alt="Logo"
-            className="object-contain h-auto w-[75%]"
-          />
-        </Link>
-        <div className="flex flex-col items-center justify-start space-y-8 mt-8  flex-1">
-          {links.map((link, i) => (
-            <Link
-              key={i}
-              to={link.path}
-              className="px-2 text-center w-full flex justify-center items-center"
-              onClick={() => setIsDrawerOpen(false)} // Added this line to close the drawer
-            >
-              {link.linkName}
-            </Link>
-          ))}
-          <button
-            onClick={toggleDarkMode}
-            className="py-1 px-2 mt-auto bg-black border border-white rounded hover:bg-gray-500 self-center"
+          <Link
+            to="/"
+            className="text-2xl font-bold mb-4 flex justify-center items-center"
           >
-            {isDarkMode ? '🌞' : '🌙'}
-          </button>
-        </div>
-        <div className="flex flex-col items-center justify-end flex-1">
-          <div className="mb-20">
-            <a
-              href="https://github.com/foleyb25"
-              target="_blank"
-              rel="noreferrer"
-              className="mr-4" // Added margin-right
+            <img
+              src={isDarkMode ? darkModeLogo : lightModeLogo}
+              alt="Logo"
+              className="object-contain h-auto w-[75%]"
+            />
+          </Link>
+          <div className="flex flex-col items-center justify-start space-y-8 mt-8  flex-1">
+            {links.map((link, i) => (
+              <Link
+                key={i}
+                to={link.path}
+                className="px-2 text-center w-full flex justify-center items-center"
+                onClick={() => setIsDrawerOpen(false)} // Added this line to close the drawer
+              >
+                {link.linkName}
+              </Link>
+            ))}
+            <button
+              onClick={toggleDarkMode}
+              className="py-1 px-2 mt-auto bg-black border border-white rounded hover:bg-gray-500 self-center"
             >
-              <FontAwesomeIcon icon={['fab', 'github']} className="text-4xl" />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/brian-foley-456624196/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon
-                icon={['fab', 'linkedin']}
-                className="text-4xl"
-              />
-            </a>
+              {isDarkMode ? '🌞' : '🌙'}
+            </button>
+          </div>
+          <div className="flex flex-col items-center justify-end flex-1">
+            <div className="mb-20">
+              <a
+                href="https://github.com/foleyb25"
+                target="_blank"
+                rel="noreferrer"
+                className="mr-4" // Added margin-right
+              >
+                <FontAwesomeIcon
+                  icon={['fab', 'github']}
+                  className="text-4xl"
+                />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/brian-foley-456624196/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon
+                  icon={['fab', 'linkedin']}
+                  className="text-4xl"
+                />
+              </a>
+            </div>
           </div>
         </div>
       </div>
